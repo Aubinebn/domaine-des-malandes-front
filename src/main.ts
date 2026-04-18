bfdb67633dafca46aeaad905413081a04c9c8bf8
Aubin EBANO <ebano.aubin@gmail.com>
2026-04-18 15:39:37 +0200

    Passage de css à scss avec sass

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 // Import global styles
-import '@/assets/styles/reset.css'
-import '@/assets/styles/font.css'
+import '@/assets/styles/reset.scss'
+import '@/assets/styles/font.scss'
 
 createApp(App).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,13 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/assets/styles/vars.scss" as *;`,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
