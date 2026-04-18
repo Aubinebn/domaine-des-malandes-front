f62fd94279b539f17c64e078618b07eba7587442
Aubin EBANO <ebano.aubin@gmail.com>
2026-04-18 15:41:57 +0200

    Création d'un layout grid

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import App from './App.vue'
 // Import global styles
 import '@/assets/styles/reset.scss'
 import '@/assets/styles/font.scss'
+import '@/assets/styles/layout.scss'
 
 createApp(App).mount('#app')
