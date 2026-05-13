e924edbf3250f03188866f5e113cf0b8fc9e8ee0
Aubin EBANO <ebano.aubin@gmail.com>
2026-05-13 20:05:50 +0200

    fix(components): Corrige erreur de syntaxe causant une erreur

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -121,7 +121,7 @@
     grid-column: 5 / 9;
     font-family: "Mrs Eaves OT";
     font-size: 24px;
-    line-height:1.1
+    line-height:1.1;
     font-style: italic;
     text-align: center;
     margin: 20px 0;
