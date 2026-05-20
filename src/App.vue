faff4ec29aa13c207001a702552d9f871f2cf25d
Aubin EBANO <ebano.aubin@gmail.com>
2026-05-20 10:49:02 +0200

    feat(animation): Ajout du scroll smooth

--- src/App.vue
+++ src/App.vue
@@ -7,19 +7,40 @@
     import Map from '@/components/Map.vue';
     import KnowHow from '@/components/KnowHow.vue';
     import Team from '@/components/Team.vue';
-    import Footer from '@/components/Footer.vue'
+    import Footer from '@/components/Footer.vue';
+
+    import {onMounted } from 'vue';
+    import gsap from 'gsap';
+    import ScrollSmoother from 'gsap/ScrollSmoother'
+
+    gsap.registerPlugin(ScrollSmoother);
+
+    onMounted(() => {
+
+        ScrollSmoother.create({
+            wrapper: '#smooth-wrapper',
+            content: '#smooth-content',
+            smooth: 2,
+            effects: true,
+        });
+    });
+
 </script>
 
 <template>
     <Menu/>
-    <HeroBanner/>
-    <Presentation/>
-    <Historic/>
-    <Wines/>
-    <Map/>
-    <KnowHow/>
-    <Team/>
-    <Footer/>
+    <div id="smooth-wrapper">
+        <div id="smooth-content">
+            <HeroBanner/>
+            <Presentation/>
+            <Historic/>
+            <Wines/>
+            <Map/>
+            <KnowHow/>
+            <Team/>
+            <Footer/>
+        </div>
+    </div>  
 </template>
 
 <style scoped></style>
